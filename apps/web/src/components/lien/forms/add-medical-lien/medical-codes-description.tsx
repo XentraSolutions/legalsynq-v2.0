960b47329e8549a2d55b2ff1f8621001f08e629f
xentra-renaecarino <renae.carino@xentragroup.com>
2026-07-23 22:45:59 +0800

    adjust edit medical code id

--- apps/web/src/components/lien/forms/add-medical-lien/medical-codes-description.tsx
+++ apps/web/src/components/lien/forms/add-medical-lien/medical-codes-description.tsx
@@ -174,26 +174,29 @@ export default function MedicalCodesDescription(
     const selectedOption = medicalCodes?.find(
       (option) => option.value === form.procedureCode,
     );
-    const nextRow = {
-      id: editingId || `temp-${Date.now()}`,
-      code: form.procedureCode,
-      description: selectedOption?.label ?? "",
-      medicareCost: parseNumber(form.medicareCost),
-      billingAmount: parseNumber(currentBilling),
-      purchaseAmount: getCurrentValue(),
-    };
 
-    setRows((current) => {
-      if (editingId) {
-        return current.map((row) => (row.id === editingId ? nextRow : row));
-      }
-      return [...current, nextRow];
-    });
-    setTimeout(() => {
-      createMedicalCodeLiens(
+    setTimeout(async () => {
+      const response = await createMedicalCodeLiens(
         { ...form, id: editingId ?? form.id },
         editingId != "",
       );
+
+      const nextRow = {
+        id: editingId || response.data,
+        code: form.procedureCode,
+        description: selectedOption?.label ?? "",
+        medicareCost: parseNumber(form.medicareCost),
+        billingAmount: parseNumber(currentBilling),
+        purchaseAmount: getCurrentValue(),
+      };
+
+      setRows((current) => {
+        if (editingId) {
+          return current.map((row) => (row.id === editingId ? nextRow : row));
+        }
+        return [...current, nextRow];
+      });
+      validateForm();
     }, 100);
     resetLine();
   }
@@ -217,7 +220,7 @@ export default function MedicalCodesDescription(
         payee: payload.payee,
         outboundCheckNumber: payload.outboundCheckNumber,
       };
-      isEditing
+      const res = isEditing
         ? await casesService.updateMedicalCodeLiens(request)
         : await casesService.createMedicalCodeLiens(request);
       addToast({
@@ -225,6 +228,7 @@ export default function MedicalCodesDescription(
         title: `Medical Code ${isEditing ? "Updated" : "Created"}`,
         description: `Medical Code has been ${isEditing ? "updated" : "created"}.`,
       });
+      return res;
     } catch (err) {
       if (err instanceof ApiError) {
         addToast({
