8db6a4cdbcc162a375af46a6c232cde873861899
xentra-renaecarino <renae.carino@xentragroup.com>
2026-09-01 03:19:00 +0800

    fix merge error

--- apps/web/src/components/lien/forms/add-medical-code.tsx
+++ apps/web/src/components/lien/forms/add-medical-code.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import { useQueryClient } from "@tanstack/react-query";
 import { FormModal } from "@/components/lien/modal";
 import { useLienStore } from "@/stores/lien-store";
 import { casesService, type CreateCaseRequestDto } from "@/lib/cases";
